1665057e84fb8262459b1a5f3a54136eaf827ca4
Alex Varghese <alex.varghese77@gmail.com>
2018-04-22 00:03:09 -0800

    to do app is ready

--- src/App.js
+++ src/App.js
@@ -4,17 +4,52 @@ import Note from './Note/note.jsx'
 import NoteForm from './NoteForm/noteForm.jsx'
 
 class App extends Component {
+
+constructor(props){
+  super(props);
+  this.state={
+    notes:[
+      {
+        noteContent:"FIrst Note",
+        noteId:"1"
+      },
+      {
+        noteContent:"Second Note",
+        noteId:"2"
+      },
+    
+    ]
+  }
+}
+
+  addnote(values){
+    debugger;
+    var previousNotes=this.state.notes;
+    var newNote=[...previousNotes,{noteContent: values, noteId: previousNotes.length+1}]
+    this.setState({
+      notes:newNote
+    })
+  }
+
   render() {
+
+   
+
+   
     return (
       <div className="App">
         <div className="head">
         TO Do
         </div>
-        <Note noteContent="FIrst Note" noteId="1"/>
-        <Note noteContent="Second Note" noteId="1"/>
-        
-        <div className="notesFooter">
-        <NoteForm/>
+        {
+          this.state.notes.map((item,i)=>{
+            return(
+              <Note noteContent={item.noteContent} noteId={item.noteId} key={i}/>
+            )
+          })
+        }  
+        <div className="footer">
+        <NoteForm addnote={this.addnote.bind(this)}/>
         </div>
       </div>
     );
--- src/NoteForm/noteForm.jsx
+++ src/NoteForm/noteForm.jsx
@@ -2,11 +2,43 @@ import React,{Component} from 'react';
 import './noteForm.css'
 
 export default class NoteForm extends Component{
+    constructor(props)
+    {
+        super(props);
+        this.state={
+            notetyped:'',
+        }
+    }
+    
+       notechanged(e){
+        var noteinput=e.target.value
+      
+         this.setState(
+          {
+                 notetyped:noteinput,
+          }
+         )
+         console.log(this.state)
+     }
+
+     addNote()
+     {
+         
+         this.props.addnote(this.state.notetyped);
+         this.setState(
+            {
+                   notetyped:"",
+            })
+     }
+
     render()
     {
+       
+     
         return(
-            <div className="noteInput">
-                 <input type="text"/>
+            <div >
+                 <input type="text" className="noteInput" placeholder="Add new note" onChange={this.notechanged.bind(this)} value={this.state.notetyped}/>
+                 <button className="button" onClick={this.addNote.bind(this)}>Add Note</button>
             </div>
         )
     }
