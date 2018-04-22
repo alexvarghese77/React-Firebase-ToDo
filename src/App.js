7f899a40ed5b56fc0d0b268c19b7ad669e63e7f9
Alex Varghese <alex.varghese77@gmail.com>
2018-04-22 05:00:25 -0800

    firebase introduced to app

--- src/App.js
+++ src/App.js
@@ -2,33 +2,44 @@ import React, { Component } from 'react';
 import './App.css';
 import Note from './Note/note.jsx'
 import NoteForm from './NoteForm/noteForm.jsx'
+import {DB_CONFIG} from './config/config.js'
+import firebase from 'firebase';
+import 'firebase/database';
 
 class App extends Component {
 
 constructor(props){
   super(props);
-  this.state={
-    notes:[
-      {
-        noteContent:"FIrst Note",
-        noteId:"1"
-      },
-      {
-        noteContent:"Second Note",
-        noteId:"2"
-      },
-    
-    ]
+  if (!firebase.apps.length) {
+  this.app=firebase.initializeApp(DB_CONFIG);
+  this.database=this.app.database().ref().child('notes');
   }
+ 
+  this.state={
+    notes:[]
+  }
+}
+componentWillMount(){
+  const previousNotes=this.state.notes;
+  this.database.on('child_added', snap => {
+    previousNotes.push({
+      id: snap.key,
+      noteContent: snap.val().noteContent,
+    })
+  })
+  this.setState({
+    notes:previousNotes
+  })
 }
 
   addnote(values){
-    debugger;
-    var previousNotes=this.state.notes;
-    var newNote=[...previousNotes,{noteContent: values, noteId: previousNotes.length+1}]
-    this.setState({
-      notes:newNote
-    })
+    // debugger;
+    // var previousNotes=this.state.notes;
+    // var newNote=[...previousNotes,{noteContent: values, noteId: previousNotes.length+1}]
+    // this.setState({
+    //   notes:newNote
+    // })
+    this.database.push().set({noteContent:values})
   }
 
   render() {
---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  export const DB_CONFIG = {
+    apiKey: "AIzaSyAMBEazqOX14PeJD2TD4Y-cw17HuDciEy0",
+    authDomain: "react-todo-26219.firebaseapp.com",
+    databaseURL: "https://react-todo-26219.firebaseio.com",
+    projectId: "react-todo-26219",
+    storageBucket: "",
+    messagingSenderId: "390445933310"
+  };
+  
